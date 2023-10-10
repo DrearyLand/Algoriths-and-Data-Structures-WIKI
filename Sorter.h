4a05925b80229d5089d0708ab9dc2f33d84efd0e
Dreary <drearyland22021@gmail.com>
2023-10-09 18:31:18 -0600

    Adding merge sort and quick sort

--- Sorter.h
+++ Sorter.h
@@ -32,7 +32,7 @@ public:
      * @param arr The vector to be sorted.
      * @param n The size of the vector.
      */
-    void bubbleSort(std::vector<int>& arr, int n) {
+    void bubbleSort(vector<int>& arr, int n) {
         comparisons = 0;
         swaps = 0;
         for (int i = 0; i < n - 1; i++) {
@@ -53,7 +53,7 @@ public:
      * @param arr The vector to be sorted.
      * @param n The size of the vector.
      */
-    void selectionSort(std::vector<int>& arr, int n) {
+    void selectionSort(vector<int>& arr, int n) {
         comparisons = 0;
         swaps = 0;
         for (int i = 0; i < n - 1; i++) {
@@ -79,7 +79,7 @@ public:
      * @param arr The vector to be sorted.
      * @param n The size of the vector.
      */
-    void insertionSort(std::vector<int>& arr, int n) {
+    void insertionSort(vector<int>& arr, int n) {
         comparisons = 0;
         swaps = 0;
         for (int i = 1; i < n; i++) {
@@ -94,6 +94,116 @@ public:
             arr[j + 1] = key;
         }
     }
+
+/**
+     * @brief Merge sort algorithm.
+     * @param arr The vector to be sorted.
+     * @param left The left index of the sub-vector.
+     * @param right The right index of the sub-vector.
+     */
+    void mergeSort(vector<int>& arr, int left, int right) {
+        
+        comparisons++;
+
+        if (left < right) {
+            // Calculate middle index of the vector
+            int middle = left + (right - left) / 2;
+
+            // Recursive call on the left sub-vector
+            mergeSort(arr, left, middle);
+
+            // Recursive call on the right sub-vector
+            mergeSort(arr, middle + 1, right);
+
+            // Merge the sorted sub-vectors
+            merge(arr, left, middle, right);
+        }
+    }
+
+    /**
+     * @brief Merge function for Merge Sort.
+     * @param arr The vector to be merged.
+     * @param left The left index of the sub-vector.
+     * @param middle The middle index of the sub-vector.
+     * @param right The right index of the sub-vector.
+     */
+    void merge(vector<int>& arr, int left, int middle, int right) {
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+
+        // Create temporary vectors
+        vector<int> leftArr(n1);
+        vector<int> rightArr(n2);
+
+        // Copy data to temporary vectors leftArr[] and rightArr[]
+        for (int i = 0; i < n1; i++) {
+            leftArr[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            rightArr[j] = arr[middle + 1 + j];
+        }
+
+        // Merge the temporary vectors back into arr[left..right]
+        int i = 0;
+        int j = 0;
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k] = leftArr[i];
+                i++;
+            } else {
+                arr[k] = rightArr[j];
+                j++;
+                swaps++;
+            }
+            k++;
+        }
+
+        // Copy the remaining elements of leftArr[], if there are any
+        while (i < n1) {
+            arr[k] = leftArr[i];
+            i++;
+            k++;
+        }
+
+        // Copy the remaining elements of rightArr[], if there are any
+        while (j < n2) {
+            arr[k] = rightArr[j];
+            j++;
+            k++;
+        }
+    }
+
+    /**
+     * @brief Shell sort algorithm.
+     * @param arr The vector to be sorted.
+     * @param n The size of the vector.
+     */
+    void shellSort(vector<int>& arr, int n) {
+        comparisons = 0;
+        swaps = 0;
+
+        // Start with a big gap, then reduce the gap
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            // Do a gapped insertion sort for this gap size.
+            // The first gap elements a[0..gap-1] are already in gapped order
+            // Keep adding one more element until the entire array is gap sorted
+            for (int i = gap; i < n; i++) {
+                int temp = arr[i];
+                int j;
+                comparisons++;
+                // Shift earlier gap-sorted elements up until the correct location for arr[i] is found
+                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                    comparisons++;
+                    arr[j] = arr[j - gap];
+                    swaps++;
+                }
+                // Put arr[i] (the original a[i]) in its correct location
+                arr[j] = temp;
+            }
+        }
+    }
+
 
     /**
      * @brief Get the number of comparisons made during sorting.
--- main.cpp
+++ main.cpp
@@ -10,40 +10,66 @@ using namespace std;
  */
 int main() {
     // Initialize data
-    vector<int> data = {12, 4, 5, 6, 7, 3, 1, 15, 8, 10};
+    vector<int> data(1000);
+    for (int i = 0; i < data.size(); i++) {
+        data[i] = rand() % 1000;
+    }
+    vector<int> aux = data;
+
     Sorter sort;
     int n = data.size();
 
     // Display original data
     cout << "Original data: ";
-    sort.mostrarVector(data);
+    //sort.mostrarVector(data);
 
     // Perform bubble sort
     cout << "\n\nStarting the bubble sort algorithm..." << endl;
     sort.bubbleSort(data,n);
     cout << "Swaps done: " << sort.getSwaps() << endl;
     cout << "Comparisons done: " << sort.getComparisons() << endl;
-    sort.mostrarVector(data);
+    //sort.mostrarVector(data);
 
     // Reset data
-    data = {12, 4, 5, 6, 7, 3, 1, 15, 8, 10};
+    data = aux;
 
     // Perform selection sort
     cout << "\nStarting the selection sort algorithm..." << endl;
     sort.selectionSort(data,n);
     cout << "Swaps done: " << sort.getSwaps() << endl;
     cout << "Comparisons done: " << sort.getComparisons() << endl;
-    sort.mostrarVector(data);
+    //sort.mostrarVector(data);
 
     // Reset data
-    data = {12, 4, 5, 6, 7, 3, 1, 15, 8, 10};
+    data = aux;
 
     // Perform insertion sort
     cout << "\nStarting the insertion sort algorithm..." << endl;
     sort.insertionSort(data,n);
     cout << "Swaps done: " << sort.getSwaps() << endl;
     cout << "Comparisons done: " << sort.getComparisons() << endl;
-    sort.mostrarVector(data);
+    //sort.mostrarVector(data);
 
+    // Reset data
+    data = aux;
+
+    // Perform insertion sort
+    cout << "\nStarting the merge sort algorithm..." << endl;
+    sort.mergeSort(data,0,n - 1);
+    cout << "Swaps done: " << sort.getSwaps() << endl;
+    cout << "Comparisons done: " << sort.getComparisons() << endl;
+    //sort.mostrarVector(data);
+
+    // Reset data
+    data = aux;
+
+    // Perform Shell sort
+    cout << "\nStarting the Shell sort algorithm..." << endl;
+    sort.shellSort(data, n);
+    cout << "Swaps done: " << sort.getSwaps() << endl;
+    cout << "Comparisons done: " << sort.getComparisons() << endl;
+    // sort.mostrarVector(data);
+
+    data.clear();
     return 0;
 }
